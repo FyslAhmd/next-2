942c4a18974506177b6a41f3b31f6e8993a32cd6
FyslAhmd <info.faysal.32@gmail.com>
2026-01-30 22:11:32 +0600

    search and filtering in SSR and url manupulation

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,23 +1,28 @@
 import FoodCard from "@/components/Cards/FoodCard";
 import CartItems from "@/components/Cart/CartItems";
+import InputSearch from "@/components/Search/InputSearch";
 import React from "react";
 
-const getFoods = async () => {
+const getFoods = async (search) => {
   const res = await fetch(
-    " https://taxi-kitchen-api.vercel.app/api/v1/foods/random"
+    `https://taxi-kitchen-api.vercel.app/api/v1/foods/random?search=${search}`,
   );
   const data = await res.json();
   return data.foods || [];
 };
 
-const FoodsPage = async () => {
-  const foods = await getFoods();
+const FoodsPage = async ({ searchParams }) => {
+  const { search = "" } = await searchParams;
+  const foods = await getFoods(search);
 
   return (
     <div>
       <h2 className="text-3xl font-bold text-center py-4">
         Total {foods.length} Foods
       </h2>
+      <div className="my-4">
+        <InputSearch></InputSearch>
+      </div>
       <div className="flex gap-5">
         <div className="flex-1 grid grid-cols-3 gap-5">
           {foods.map((food) => (
